d0a4c6fc834f3e46271fbde135be76b7b971b7bc
jdelauney <delauney.jerome@gmail.com>
2023-09-19 09:28:15 +0200

    ♻️(RenderModal) : Auto create html div wrapper

--- src/components/RenderModal/RenderModal.tsx
+++ src/components/RenderModal/RenderModal.tsx
@@ -1,8 +1,41 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useLayoutEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-export const RenderModal = (props: PropsWithChildren<{}>) => {
-  const { children } = props;
+type RenderModalProps = PropsWithChildren<{
+  wrapperId: string;
+}>;
+export const RenderModal = (props: RenderModalProps) => {
+  const { children, wrapperId } = props;
 
-  return createPortal(children, document.getElementById('modal-root')!);
+  if (wrapperId === '') throw new Error('wrapperId is required and cannot be empty');
+
+  const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(null);
+
+  const createModalWrapper = (wrapperId: string) => {
+    const wrapperElement = document.createElement('div');
+    wrapperElement.setAttribute('id', wrapperId);
+    document.body.appendChild(wrapperElement);
+    return wrapperElement;
+  };
+
+  useLayoutEffect(() => {
+    let element = document.getElementById(wrapperId);
+    let systemCreated = false;
+
+    if (!element) {
+      systemCreated = true;
+      element = createModalWrapper(wrapperId);
+    }
+    setWrapperElement(element);
+
+    return () => {
+      if (systemCreated && element !== null && element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    };
+  }, [wrapperId]);
+
+  if (wrapperElement === null) return null;
+
+  return createPortal(children, wrapperElement);
 };
