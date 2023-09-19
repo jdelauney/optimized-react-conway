8aa782b63b22acb9f07f7e936b4055e2c6881798
jdelauney <delauney.jerome@gmail.com>
2023-09-19 09:07:09 +0200

    🐛 : Fix wrong display cells

--- src/engine/conway.ts
+++ src/engine/conway.ts
@@ -27,6 +27,7 @@ export class ConwayEngine {
   private gameBoard!: Uint8Array;
   private lastBoard!: Uint8Array;
   private nbNeighbors!: Uint8Array;
+  private decompositionTime!: Uint8Array;
   private neighborsDeltaIndex!: number[];
   private precalcCellPos!: Position2D[];
   private canvasRef: HTMLCanvasElement | null;
@@ -65,8 +66,8 @@ export class ConwayEngine {
       -1, // left
       +1, // right
       -this.maxCol, // Top
-      -(this.maxCol + 1), // TopLeft
-      -(this.maxCol - 1), // TopRight
+      -(this.maxCol - 1), // TopLeft
+      -(this.maxCol + 1), // TopRight
       +this.maxCol, // bottom
       +(this.maxCol - 1), // BottomLeft
       +(this.maxCol + 1), // BottomRight
@@ -78,6 +79,7 @@ export class ConwayEngine {
     this.nextBoard = new Uint8Array(this.maxCell);
     this.lastBoard = new Uint8Array(this.maxCell);
     this.nbNeighbors = new Uint8Array(this.maxCell);
+    this.decompositionTime = new Uint8Array(this.maxCell).fill(0);
     this.precalcCellPos = new Array();
 
     this.prepareBufferCellPosFromIndex();
@@ -96,7 +98,6 @@ export class ConwayEngine {
     for (let i = 0; i < this.maxCell; i++) {
       const x = Math.floor(i % this.maxCol);
       const y = Math.floor(i * this.reciprocalMaxCol);
-      //this.precalcCellPos[i] = { x, y };
       this.precalcCellPos.push({ x, y });
     }
   }
@@ -116,7 +117,7 @@ export class ConwayEngine {
   private getLivingCells() {
     let livingCells = [];
     for (let i = 0; i < this.maxCell; i++) {
-      if (this.gameBoard[i] == 1) {
+      if (this.gameBoard[i] === 1) {
         livingCells.push(i);
       }
     }
@@ -126,25 +127,62 @@ export class ConwayEngine {
   private computeCellsToLive() {
     this.cellAlive = 0;
     for (let i = 0; i < this.maxCell; i++) {
-      const isAlive = this.gameBoard[i] == 1;
+      const isAlive = this.gameBoard[i] === 1;
       const needLive =
         (isAlive && (this.nbNeighbors[i] === 2 || this.nbNeighbors[i] === 3)) ||
         (!isAlive && this.nbNeighbors[i] === 3);
       if (needLive) {
         this.cellAlive++;
+        this.decompositionTime[i] = 100;
+      } else if (this.decompositionTime[i] > 0) {
+        this.decompositionTime[i] -= 25;
       }
       this.nextBoard[i] = needLive ? 1 : 0;
     }
   }
+
+  private adjustColorIntensity(hexColor: string, intensity: number) {
+    // Convertir la couleur hexadécimale en RGB
+    let r: number = parseInt(hexColor.substr(1, 2), 16);
+    let g: number = parseInt(hexColor.substr(3, 2), 16);
+    let b: number = parseInt(hexColor.substr(5, 2), 16);
+
+    // Ajuster chaque composante de couleur en fonction de l'intensité
+    r = Math.floor(r * intensity);
+    g = Math.floor(g * intensity);
+    b = Math.floor(b * intensity);
+
+    // Assurez-vous que chaque composante est dans l'intervalle [0,255]
+    r = Math.max(0, Math.min(255, r));
+    g = Math.max(0, Math.min(255, g));
+    b = Math.max(0, Math.min(255, b));
+
+    // Convertir chaque composante en hexadécimal et combiner les résultats
+    const rs: string = r.toString(16).padStart(2, '0');
+    const gs: string = g.toString(16).padStart(2, '0');
+    const bs: string = b.toString(16).padStart(2, '0');
+
+    return '#' + rs + gs + bs;
+  }
+
+  // private adjustColorIntensity(hexColor: string, intensity: number): string {
+  //   const rgb: string[] = [0, 2, 4].map(shift => {
+  //     let component: number = parseInt(hexColor.substring(1 + shift, 3 + shift), 16);
+  //     component = Math.floor(component * intensity);
+  //     component = Math.max(0, Math.min(255, component));
+
+  //     return component.toString(16).padStart(2, '0');
+  //   });
+
+  //   return '#' + rgb.join('');
+  // }
 
   public genNextStep() {
     // On récupère uniquement les cellules vivantes
     let livingCells = this.getLivingCells();
 
     // Reset des voisins
-    for (let i = 0; i < this.maxCell; i++) {
-      this.nbNeighbors[i] = 0;
-    }
+    this.nbNeighbors.fill(0);
 
     // On mets a jour le tableau des voisins
     for (let i = 0; i < livingCells.length; i++) {
@@ -166,44 +204,50 @@ export class ConwayEngine {
     if (this.canvasRef === null || this.ctx === null) {
       throw new Error('canvasRef or canvas Ctx must not be null');
     }
-
     this.ctx.save();
     this.ctx.beginPath();
+    //this.ctx.clearRect(0, 0, this.canvasRef.width, this.canvasRef.height);
 
-    // Efface le canvas
-    this.ctx.clearRect(0, 0, this.canvasRef.width, this.canvasRef.height);
-
-    this.ctx.fillStyle = this.colorCell;
-
-    this.cellAlive = 0;
     for (let i = 0; i < this.maxCell; i++) {
       const newValue = this.gameBoard[i];
-      if (newValue === 1) {
-        this.cellAlive++;
+      const { x, y } = this.precalcCellPos[i];
+
+      const px = x * this.cellSizeInPx;
+      const py = y * this.cellSizeInPx;
+      // Dessiner les cellules mortes avec une couleur basée sur leur temps de décomposition
+      if (this.lastBoard[i] === 0 && newValue === 0 && this.decompositionTime[i] >= 0) {
+        const intensity = this.decompositionTime[i] * 0.01; // 1/100
+        this.ctx.fillStyle = this.adjustColorIntensity(this.colorCell, intensity);
+        this.ctx.fillRect(px, py, this.cellSizeInPx, this.cellSizeInPx);
       }
-      if (this.lastBoard[i] !== newValue) {
-        if (newValue === 1) {
-          const { x, y } = this.precalcCellPos[i];
-          this.ctx.fillRect(x * this.cellSizeInPx, y * this.cellSizeInPx, this.cellSizeInPx, this.cellSizeInPx);
-        }
+
+      // Dessiner les cellules qui sont passées de mortes à vivantes
+      if (this.lastBoard[i] === 0 && newValue === 1) {
+        this.ctx.fillStyle = this.colorCell;
+        this.ctx.fillRect(px, py, this.cellSizeInPx, this.cellSizeInPx);
       }
     }
 
-    // Dessine les lignes
     if (this.showGridLines) {
       this.ctx.lineWidth = 1;
       this.ctx.strokeStyle = this.colorGrid;
+
       for (let i = 0; i <= this.maxCol; i++) {
-        this.ctx.moveTo(i * this.cellSizeInPx, 0);
-        this.ctx.lineTo(i * this.cellSizeInPx, this.displayHeight);
+        const delta = i * this.cellSizeInPx;
+        this.ctx.moveTo(delta, 0);
+        this.ctx.lineTo(delta, this.displayHeight);
+        this.ctx.stroke();
       }
+
       for (let i = 0; i <= this.maxRow; i++) {
-        this.ctx.moveTo(0, i * this.cellSizeInPx);
-        this.ctx.lineTo(this.displayWidth, i * this.cellSizeInPx);
+        const delta = i * this.cellSizeInPx;
+        this.ctx.moveTo(0, delta);
+        this.ctx.lineTo(this.displayWidth, delta);
+        this.ctx.stroke();
       }
-      this.ctx.stroke();
     }
 
+    this.ctx.closePath();
     this.ctx.restore();
 
     this.lastBoard = this.gameBoard.slice(0);
@@ -262,6 +306,7 @@ export class ConwayEngine {
       this.gameBoard[i] = (await this.randomBooleanWeighted(fillRateInPercent)) ? 1 : 0;
       if (this.gameBoard[i] === 1) {
         this.cellAlive++;
+        this.decompositionTime[i] = 100;
       }
     }
     return Promise.resolve();
