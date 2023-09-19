2630788b2442d55e24cfe4d58ac98c1660fda1a8
jdelauney <delauney.jerome@gmail.com>
2023-09-19 11:44:07 +0200

    🐛 (Navbar): Fix Settings Dialog Open state

--- src/pages/components/Navbar/Navbar.tsx
+++ src/pages/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import { useContext, useEffect, useState } from 'react';
 import { Button } from '../../../components/ui/Button/Button';
 import { ConwayContext } from '../../../contexts/ConwayContext';
+import { RenderModal } from '../../../components/Modal/RenderModal';
+import { ConwaySettingsModal } from '../ConwaySettingsModal/ConwaySettingsModal';
 
 export const Navbar = () => {
   const { conwayEngine, setIsRunning, isRunning, nbGenerations, isReady, averageElapsedTime, needUpdate } =
     useContext(ConwayContext);
+
   const [totalCell, setTotalCell] = useState<number>(0);
   const [cellAlive, setCellAlive] = useState<number>(0);
+  const [isSettingsDialogOpen, setIsSettingsDialogOpen] = useState<boolean>(false);
 
   useEffect(() => {
     if (conwayEngine) {
@@ -22,36 +26,55 @@ export const Navbar = () => {
     });
   };
 
-  return (
-    <div className={'relative'}>
-      <nav className='fixed top-0 left-0 right-0 w-full flex justify-end items-center py-3 px-4 gap-3 bg-slate-800/60'>
-        <span className='text-2xl leading-none font-bold text-gray-100 border-e-2 border-slate-400 mr-2 pr-2'>
-          Conway's Game of Life
-        </span>
-        <div className='flex-grow flex items-center divide-x-2 divide-slate-300'>
-          <span className='leading-none font-bold text-gray-100 mr-2 pr-2 text-sm'>
-            Total cells : <span className='text-blue-300 font-normal'>{isNaN(totalCell) ? '-' : totalCell}</span>
-          </span>
-          <span className='leading-none font-bold text-gray-100 px-2 text-sm'>
-            Generation : <span className='text-yellow-400 font-normal'>{nbGenerations}</span>
-          </span>
-          <span className='leading-none font-bold text-gray-100 px-2 text-sm'>
-            Cells alive : <span className='text-green-300 font-normal'>{cellAlive}</span>
-          </span>
-          <span className='leading-none font-bold text-gray-100 px-2 text-sm'>
-            Average speed rendering :{' '}
-            <span className='text-red-200 font-normal'>{averageElapsedTime.toFixed(3)}ms</span>
-          </span>
-        </div>
+  const handleSettingsClick = () => {
+    setIsSettingsDialogOpen(currentState => {
+      currentState = !currentState;
+      return currentState;
+    });
+  };
 
-        <Button variant='primary' onClick={handlePlayClick}>
-          {isRunning ? 'Stop' : 'Play'}
-        </Button>
-        <Button variant='primary'>Next step</Button>
-        <Button variant='accent'>Setup</Button>
-        <Button variant='danger'>Reset</Button>
-        <Button variant='primary'>Random</Button>
-      </nav>
-    </div>
+  const handleModalClose = () => {
+    setIsSettingsDialogOpen(false);
+  };
+
+  return (
+    <>
+      <div className={'relative'}>
+        <nav className='fixed top-0 left-0 right-0 w-full flex justify-end items-center py-3 px-4 gap-3 bg-slate-800/60'>
+          <span className='text-2xl leading-none font-bold text-gray-100 border-e-2 border-slate-400 mr-2 pr-2'>
+            Conway's Game of Life
+          </span>
+          <div className='flex-grow flex items-center divide-x-2 divide-slate-300'>
+            <span className='leading-none font-bold text-gray-100 mr-2 pr-2 text-sm'>
+              Total cells : <span className='text-blue-300 font-normal'>{isNaN(totalCell) ? '-' : totalCell}</span>
+            </span>
+            <span className='leading-none font-bold text-gray-100 px-2 text-sm'>
+              Generation : <span className='text-yellow-400 font-normal'>{nbGenerations}</span>
+            </span>
+            <span className='leading-none font-bold text-gray-100 px-2 text-sm'>
+              Cells alive : <span className='text-green-300 font-normal'>{cellAlive}</span>
+            </span>
+            <span className='leading-none font-bold text-gray-100 px-2 text-sm'>
+              Average speed rendering :{' '}
+              <span className='text-red-200 font-normal'>{averageElapsedTime.toFixed(3)}ms</span>
+            </span>
+          </div>
+
+          <Button variant='primary' onClick={handlePlayClick}>
+            {isRunning ? 'Stop' : 'Play'}
+          </Button>
+          <Button variant='primary'>Next step</Button>
+          <Button variant='accent' onClick={handleSettingsClick}>
+            Setup
+          </Button>
+          <Button variant='danger'>Reset</Button>
+          <Button variant='primary'>Random</Button>
+        </nav>
+      </div>
+
+      <RenderModal>
+        {isSettingsDialogOpen && <ConwaySettingsModal isOpen={isSettingsDialogOpen} onClose={handleModalClose} />}
+      </RenderModal>
+    </>
   );
 };
