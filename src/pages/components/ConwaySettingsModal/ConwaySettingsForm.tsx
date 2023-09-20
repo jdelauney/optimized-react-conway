6bc5c99824908f413ee9179a7156c6d2008c6be2
jdelauney <delauney.jerome@gmail.com>
2023-09-20 12:00:03 +0200

    🐛(ConwaySettingsForm): Fix submit

--- src/pages/components/ConwaySettingsModal/ConwaySettingsForm.tsx
+++ src/pages/components/ConwaySettingsModal/ConwaySettingsForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent } from 'react';
+import { useContext, useState } from 'react';
 import {
   AiOutlineBgColors,
   GiPerspectiveDiceSixFacesRandom,
@@ -7,34 +7,111 @@ import {
 } from '../../../components/ui/Icons/Icons';
 import { Input } from '../../../components/ui/Inputs/Input';
 import { Switch } from '../../../components/ui/Inputs/Switch';
+import { ConwayContext } from '../../../contexts/ConwayContext';
+import { ConwaySettingsType } from '../../../engine/conway';
 
 type ConwaySettingsFormProps = {
-  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
-  onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  id: string;
+  //   onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+  //   onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  // settings: ConwaySettingsType | null;
 };
 
 export const ConwaySettingsForm = (props: ConwaySettingsFormProps) => {
-  const { onSubmit, onInputChange } = props;
+  //const { onSubmit, onInputChange } = props;
+
+  const { conwaySettings, setConwaySettings, backgroundColor, setBackgroundColor, setIsReady } =
+    useContext(ConwayContext);
+  const [bgColor, setBgColor] = useState<string>(backgroundColor);
+  const [newSettings, setNewSettings] = useState<ConwaySettingsType>(conwaySettings);
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Submit');
+    console.log('New ConwaySettings : ', newSettings);
+
+    setConwaySettings(newSettings);
+    setBackgroundColor(previousState => {
+      previousState = bgColor;
+      return previousState;
+    });
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    let newValue: string | boolean | number = value;
+    // let canUpdate = true;
+
+    if (name === 'backgroundColor') {
+      setBgColor(newValue);
+      return;
+    }
+
+    if (name === 'showGridLines') {
+      console.log('Input change ===> : ', name, e.target.checked);
+
+      newValue = e.target.checked;
+    }
+    console.log('Input change : ', name, newValue);
+    setNewSettings({
+      ...newSettings,
+      [name]: newValue,
+    });
+
+    //   return;
+    // }
+  };
+
   return (
-    <form className={'flex flex-col gap-3 w-full'} onSubmit={onSubmit}>
-      <Input type='number' name='cellSize' label='Cell size' Icon={<MdOutlinePhotoSize />} onChange={onInputChange} />
+    <form id={props.id} className={'flex flex-col gap-3 w-full'} onSubmit={handleFormSubmit}>
+      <Input
+        type='number'
+        name='cellSize'
+        label='Cell size'
+        value={newSettings.cellSize}
+        min={3}
+        max={24}
+        Icon={<MdOutlinePhotoSize />}
+        onChange={handleInputChange}
+      />
       <Input
         type='number'
         name='randomFillRate'
         label='Random fill rate'
+        value={newSettings.randomFillRate}
         Icon={<GiPerspectiveDiceSixFacesRandom />}
-        onChange={onInputChange}
+        onChange={handleInputChange}
       />
       <Input
         type='color'
-        name='bgColor'
+        name='backgroundColor'
         label='Background color'
+        value={bgColor}
         Icon={<AiOutlineBgColors />}
-        onChange={onInputChange}
+        onChange={handleInputChange}
       />
-      <Input type='color' name='cellColor' label='Cell color' Icon={<VscSymbolColor />} onChange={onInputChange} />
-      <Switch name='ShowGridLines' label='Show grid lines' onChange={onInputChange} />
-      <Input type='color' name='gridColor' label='Grid color' Icon={<VscSymbolColor />} onChange={onInputChange} />
+      <Input
+        type='color'
+        name='cellColor'
+        label='Cell color'
+        value={newSettings.cellColor}
+        Icon={<VscSymbolColor />}
+        onChange={handleInputChange}
+      />
+      <Switch
+        name='showGridLines'
+        label='Show grid lines'
+        checkState={newSettings.showGridLines}
+        onChange={handleInputChange}
+      />
+      <Input
+        type='color'
+        name='gridColor'
+        label='Grid color'
+        value={newSettings.gridColor}
+        Icon={<VscSymbolColor />}
+        onChange={handleInputChange}
+      />
     </form>
   );
 };
