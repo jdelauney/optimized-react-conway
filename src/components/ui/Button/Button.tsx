083dfe54f3f652a2fe40c7202b69e2dcf78079ef
jdelauney <delauney.jerome@gmail.com>
2023-09-20 11:53:24 +0200

    ♻️(Button) : Add disabled style

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,20 +1,26 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from "react"
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 
-type ButtonVariant = "primary" | "secondary" | "accent" | "danger" | "warning" | "success" | "info"
+type ButtonVariant = 'primary' | 'secondary' | 'accent' | 'danger' | 'warning' | 'success' | 'info';
 type ButtonProps = PropsWithChildren<{
-  variant: ButtonVariant
-}> & ButtonHTMLAttributes<HTMLButtonElement>;
+  variant: ButtonVariant;
+}> &
+  ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = (props: ButtonProps) => {
   const { variant, children, ...rest } = props;
   const buttonBaseStyle = `inline-block rounded px-5 pb-1.5 pt-1 text-sm font-medium uppercase leading-none
   shadow shadow-slate-700 transition duration-150 ease-in-out
   hover:shadow-none focus:shadow-sm focus:outline-none focus:ring-0
-  `
+  disabled:opacity-50 disabled:cursor-not-allowed disabled:shadow-none disabled:ring-0 disabled:hover:shadow-none disabled:focus:shadow-sm disabled:focus:ring-0
+  disabled:bg-gray-500 disabled:text-gray-100 disabled:border-gray-600
+  `;
 
   return (
-    <button className={`${buttonBaseStyle} bg-${variant}-500 text-${variant}-100 border border-${variant}-600  hover:bg-${variant}-700  hover:border-${variant}-500 focus:bg-${variant}-600  focus:border-${variant}-500 `} {...rest}>
+    <button
+      className={`${buttonBaseStyle} bg-${variant}-500 text-${variant}-100 border border-${variant}-600  hover:bg-${variant}-700  hover:border-${variant}-500 focus:bg-${variant}-600  focus:border-${variant}-500 `}
+      {...rest}
+    >
       {children}
     </button>
-  )
-}
+  );
+};
